ea2158801ba9ae6e3f5eff5da1ba8dd8240e9bae
gmn26 <ghazynovrial@gmail.com>
2024-11-24 00:07:29 +0700

    update : adjustment for github page

--- src/sections/Section3.jsx
+++ src/sections/Section3.jsx
@@ -11,12 +11,22 @@ export default function Section3() {
       repeat: Infinity,
       ease: "bounce",
     });
+
+    gsap.to(".animate-box-2", {
+      rotate: -360,
+      duration: 5,
+      x: -1000,
+      yoyo: true,
+      repeat: Infinity,
+      ease: "bounce",
+    });
   });
 
   return (
     <section className="relative">
       <div className="animate-box w-24 h-24 border border-red-600 absolute top-0 left-0"></div>
-      <h2 className="box">Front End</h2>
+      <div className="animate-box-2 w-24 h-24 border border-red-600 absolute bottom-0 right-0"></div>
+      <h2 className="box">Coming Soon</h2>
     </section>
   );
 }
