a139b8b5d0d55aea18017ed53e0ee803f3779b59
gmn26 <ghazynovrial@gmail.com>
2025-05-15 15:56:49 +0700

    feat : add tech stack icons

--- src/sections/Section4.jsx
+++ src/sections/Section4.jsx
@@ -1,4 +1,20 @@
 import { useState } from "react";
+import react from "../assets/react.svg";
+import typescript from "../assets/typescript.svg";
+import tailwind from "../assets/tailwind.svg";
+import bootstrap from "../assets/bootstrap.svg";
+import angular from "../assets/angular.svg";
+import next from "../assets/next.svg";
+import laravel from "../assets/laravel.svg";
+import php from "../assets/php.svg";
+import express from "../assets/express.svg";
+import spring from "../assets/spring.svg";
+import mysql from "../assets/mysql.svg";
+import node from "../assets/node.svg";
+import postgresql from "../assets/postgresql.svg";
+import mongodb from "../assets/mongodb.svg";
+import prisma from "../assets/prisma.svg";
+import flutter from "../assets/flutter.svg";
 
 export default function Section4() {
     const [hoveredIndex, setHoveredIndex] = useState(null);
@@ -19,6 +35,56 @@ export default function Section4() {
                         {text}
                     </p>
                 ))}
+            </div>
+            <div className="relative flex justify-between gap-2 *:w-16">
+                <div className={`floating-effect ${hoveredIndex == 1 && "blur"}`}>
+                    <img src={react} alt="React" />
+                </div>
+                <div className={`floating-effect ${hoveredIndex == 1 && "blur"}`}>
+                    <img src={typescript} alt="Type Script" />
+                </div>
+                <div className={`floating-effect ${hoveredIndex == 1 && "blur"}`}>
+                    <img src={tailwind} alt="Tailwind" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 1 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={bootstrap} alt="Bootstrap" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 1 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={angular} alt="Angular" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 1 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={next} alt="Next" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={laravel} alt="Laravel" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={php} alt="PHP" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={express} alt="Express" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={spring} alt="Spring" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={mysql} alt="MySQL" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={node} alt="Node" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={postgresql} alt="PostgreSQL" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={mongodb} alt="MongoDB" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 2) && "blur"}`}>
+                    <img src={prisma} alt="Prisma" />
+                </div>
+                <div className={`floating-effect ${(hoveredIndex == 0 || hoveredIndex == 1) && "blur"}`}>
+                    <img src={flutter} alt="Flutter" />
+                </div>
             </div>
         </section>
     )
